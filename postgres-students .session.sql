be7f8212acbd6a2da2561a574fc994d5d37a40d8
MaryyBo <marusya.bodrug@gmail.com>
2024-04-23 14:13:03 +0200

    Data types + numeric

--- postgres-students .session.sql
+++ postgres-students .session.sql
@@ -1,13 +1,23 @@
-CREATE TABLE users_with_emails();
-
-DROP TABLE  users_with_emails, test; 
-
-
+DROP TABLE users;
 CREATE TABLE users (
-    first_name varchar(64),
-    last_name varchar(64),
-    biography text
-); 
+    first_name varchar (64),
+    last_name varchar (64),
+    biography text,
+    gender varchar (30),
+    is_subscribe boolean,
+    birthday date,
+    foot_size smallint,
+    height numeric(5, 2)
+)
 
+/*
+ numeric ( точність, масштаб)
+ точність - загальна кількість цифр в числі
+ масштаб -  кфлькфсть цифр після коми
+ 
+ 180.55
+ точність - 5 цифр
+ масштаб - 2 цифри
+ */
 
-
+ 
