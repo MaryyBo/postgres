9bd32760f959b6b122bdca4dbbc4ee7e6a95b5b2
MaryyBo <marusya.bodrug@gmail.com>
2024-04-23 14:01:48 +0200

    Creating table

--- postgres-students .session.sql
+++ postgres-students .session.sql
@@ -1,2 +1,13 @@
-CREATE TABLE test();
+CREATE TABLE users_with_emails();
+
+DROP TABLE  users_with_emails, test; 
+
+
+CREATE TABLE users (
+    first_name varchar(64),
+    last_name varchar(64),
+    biography text
+); 
+
+
 
