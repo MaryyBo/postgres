ec769144939d495456d0b0d903bdcc18321a3db4
MaryyBo <marusya.bodrug@gmail.com>
2024-04-23 15:22:25 +0200

    CONSTRAINT (Обмеження!)

--- postgres-students .session.sql
+++ postgres-students .session.sql
@@ -1,24 +1,34 @@
 
 CREATE TABLE users (
-    first_name varchar (64),
-    last_name varchar (64),
-    biography text,
-    gender varchar (30),
-    is_subscribe boolean,
-    birthday date,
+    first_name varchar (64) NOT NULL CONSTRAINT first_name_not_empty CHECK(first_name != ''),
+    last_name varchar (64) NOT NULL CONSTRAINT last_name_not_empty CHECK(last_name != ''),
+    email varchar(64) CONSTRAINT email_not_empty CHECK(email != '') UNIQUE,
+    gender varchar (30) NOT NULL CONSTRAINT gender_not_empty CHECK(gender != ''),
+    is_subscribe boolean NOT NULL,
+    birthday date CHECK (birthday <= current_date),
     foot_size smallint,
-    height numeric(5, 2)
+    height numeric(5, 2) CONSTRAINT too_high_user CHECK (height < 2.4)
 )
 
---Як вставити дані в таблицю?
+DROP TABLE users;
 
-INSERT INTO users VALUES ('John', 'Doe', 'Тут може бути опис', 'male', 'true', '2004-09-14', 43, 1.75);
+-- мусорний запис 
 
--- Декілька даних одночасно 
+INSERT INTO users VALUES (NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+-- дані 
 
 INSERT INTO users VALUES 
-('Susan', 'Doe', 'Тут може бути опис', 'female', 'true', '2001-01-19', 39, 1.55),
-('Peter', 'Doe', 'Тут може бути опис', 'male', 'true', '2002-05-18', 40, 1.95);
+('Susan', 'Doe', 'doesusan@gmail.com', 'female', 'true', '2001-01-19', 39, 1.55),
+('Peter', 'Doe', 'doeggsusan@gmail.com', 'male', 'false', '2002-05-18', 40, 1.95);
 
+INSERT INTO users VALUES ('Karen', 'Doe', 'doe@gmail.com', 'female', 'true', NULL, NULL, 2.3);
 
-INSERT INTO users VALUES ('Blake', 'Doe', 'Тут може бути опис', 'male', 'false', '2004-09-14', NULL, NULL);
+INSERT INTO users VALUES ('Oleh', 'Doe', 'doeddf@gmail.com', 'female', 'true', '2024-04-06', NULL, 1.78);
+
+-- Легальний запис, але теж мусорний
+
+INSERT INTO users VALUES ('', '', '', '', 'true', '1990-12-12', NULL, NULL); -- з Boolean не пройде
+
+-- CONSTRAINT (Обмеження!)
+
