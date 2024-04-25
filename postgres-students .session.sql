c559f3c05d8357e4a7365706dc0cfc46f0c874cb
MaryyBo <marusya.bodrug@gmail.com>
2024-04-25 12:19:10 +0200

    Складені первинні ключі( composite primary key)

--- postgres-students .session.sql
+++ postgres-students .session.sql
@@ -1,22 +1,27 @@
-CREATE TABLE messages (
-    id serial PRIMARY KEY,
-    body text NOT NULL CHECK (body != ''),
-    author varchar (256) NOT NULL CHECK (author != ''),
-    created_at timestamp DEFAULT current_timestamp,
-    is_read boolean DEFAULT false
-) 
-
-DROP TABLE messages;
-
--- INSERT INTO messages VALUES ('Hello John', 'Me'); цей запис вже не спрацює!
-
-INSERT INTO messages(author, body) VALUES 
-    ('John', 'Hello'),
-    ('Me', 'Go for coffee'),
-    ('John', 'Go');
-
-INSERT INTO messages (id, body, author)
-VALUES ( 3, 'message text 4', 'author4');
-
 --Ключ - ознака (штучна) яка відрізняє один запис у таблиці від іншого
 --Первинний ключ (PRIMARY KEY) використов для тог, щоб ключу дати обмеження унікальності і обмеження NOT NULL
+
+/*Первинні ключі на 2 стовпці*/
+
+
+CREATE TABLE coordinates (
+    x int,
+    y int,
+    z int,
+    CONSTRAINT "unique coord" PRIMARY KEY(x,y,z)
+);
+
+--(111,22,36)
+--(252,76,88)
+--(111,22,36) <---- тут має повернутися помилка
+
+INSERT INTO coordinates VALUES 
+(111,22,36),
+(252,76,88);
+
+INSERT INTO coordinates VALUES 
+(111,22,36); -- Повернеться помилка
+
+INSERT INTO coordinates VALUES 
+(231,45,56),
+(252,76,88); -- також звалимось на помилці на етапі аналізу
