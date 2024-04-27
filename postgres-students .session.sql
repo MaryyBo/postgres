d054d9a8e1f1a4719489012ff840c873a82c96cf
MaryyBo <marusya.bodrug@gmail.com>
2024-04-27 19:19:38 +0200

    Зв'язок 1:1

--- postgres-students .session.sql
+++ postgres-students .session.sql
@@ -1,42 +1,15 @@
-/* 
-Задача : Міні ЮТУБ 
+-- Зв'язок один до одного 1:1
 
-Таблиця контенту:
--назва
--опис
--автор (юзер, який створив контент)
--дата створення
-
-Таблиця Реакції:
-
--is_liked:
-    - null - не ставив оцінку
-    - true - лайк
-    - false - дизлайк
-
-
-У контента може бути багато реакцій від користувачів
-
- Реакції - зв'язки між користувачем та контентом   
-
-*/
-
-CREATE TABLE contents (
+CREATE TABLE coaches (
     id serial PRIMARY KEY,
-    name varchar(256) NOT NULL CHECK (name != ''),
-    description text,
-    author_id int REFERENCES users (id),
-    created_at timestamp DEFAULT current_timestamp
+    name varchar (256) CHECK (name !='') NOT NULL
 )
 
-CREATE TABLE reactions (
-    content_id int REFERENCES contents(id),
-    user_id int REFERENCES users (id),
-    is_liked boolean
+CREATE TABLE teams (
+    id serial PRIMARY KEY,
+    name varchar (256) CHECK (name !='') NOT NULL,
+    coach_id int  REFERENCES coaches (id)
 )
 
-INSERT INTO contents(name, author_id) VALUES --доравання контакту
-('Funny dogs', 3);
-
-INSERT INTO reactions  VALUES --доравання реакцій
-(1, 1, true);
+ALTER TABLE coaches
+ADD COLUMN team_id int REFERENCES teams (id)
