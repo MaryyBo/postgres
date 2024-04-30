b0e6310bd8bbfff3d7c3b2cf73eff5c3d0f9d038
MaryyBo <marusya.bodrug@gmail.com>
2024-04-30 08:34:09 +0200

    Cascade delete, update

--- postgres-students .session.sql
+++ postgres-students .session.sql
@@ -1,15 +1,25 @@
--- Зв'язок один до одного 1:1
+-- Ключ - ознака (найчастіше штучна) яка відрізняє один запис у таблиці від іншого
+-- Первинний ключ (PRIMARY KEY) - використовується для того, щоб ключу дати обмеження унікальності (UNIQUE) і обмеження NOT NULL
+-- Потенційний ключ - стовпець (група стовпців) які могли стати первинним ключем, але ще не обрані як такі.
+-- Зовнішній ключ (foreign key) - стовпець (група стовпців) які містять значення, які посилаються на ідентифікатори в інших таблицях
+-- Каскадне видалення - якщо ви видаляєте запис у батьківській таблиці, то автоматично будуть видалені всі записи у дочірніх таблицях, які мають посилання на цей видалений запис.
+-- Каскадне оновлення - якщо значення у записі батьківської таблиці змінюється, то всі відповідні записи у дочірніх таблицях автоматично змінюються згідно цих змін
 
-CREATE TABLE coaches (
-    id serial PRIMARY KEY,
-    name varchar (256) CHECK (name !='') NOT NULL
-)
+CREATE TABLE orders(
+    ...
+    user_id int REFERENCES user(id) ON UPDATE CASCADE ON DELETE RESTRICT 
+    ...
+);
 
-CREATE TABLE teams (
-    id serial PRIMARY KEY,
-    name varchar (256) CHECK (name !='') NOT NULL,
-    coach_id int  REFERENCES coaches (id)
-)
+DROP TABLE reactions;
 
-ALTER TABLE coaches
-ADD COLUMN team_id int REFERENCES teams (id)
+CREATE TABLE reactions(
+    content_id int REFERENCES contents(id) ON DELETE CASCADE,
+    user_id int REFERENCES users(id),
+    is_liked boolean
+);
+
+INSERT INTO reactions VALUES
+(1, 1, false);
+
+DELETE FROM contents WHERE id = 1;
