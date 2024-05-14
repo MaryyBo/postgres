1fa4dd865334d62e415269cb9e1444218928d778
MaryyBo <marusya.bodrug@gmail.com>
2024-05-14 16:41:00 +0200

    Create SCHEMA

--- conditions-and-views.sql
+++ conditions-and-views.sql
@@ -1,34 +1,8 @@
-/*
 
-order -> status:
-    -true = виконано
-    -false = не виконано
-    - ('new', 'processing', 'shipped', 'done', 'canceled')
-    
-*/
+CREATE SCHEMA new_schema;
 
-SELECT * FROM orders
-WHERE status = true;
-
-INSERT INTO orders (customer_id, status) VALUES
-(2501, 'new');
-
-CREATE TYPE order_status AS ENUM('new', 'processing', 'shipped', 'done', 'canceled');
-
-ALTER TABLE orders
-ALTER COLUMN status 
-TYPE order_status
-USING (
-    CASE status
-            WHEN false THEN 'processing'
-            WHEN true THEN 'done'
-            ELSE 'new' 
-    END 
-)::order_status;
-
-SELECT * FROM orders
-ORDER BY created_at DESC;
-
-UPDATE orders
-SET status = 'shipped'
-WHERE id = 1
+CREATE TABLE new_schema.users(
+    first_name varchar (20) NOT NULL CHECK (first_name != ''),
+    last_name varchar (20) NOT NULL CHECK (last_name != ''),
+    email varchar (20) NOT NULL CHECK (email != '')
+);
