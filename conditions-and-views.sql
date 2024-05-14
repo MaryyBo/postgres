94625231777c7953f56b90ea720b6bd44ea79e97
MaryyBo <marusya.bodrug@gmail.com>
2024-05-14 13:53:03 +0200

    Materialized views

--- conditions-and-views.sql
+++ conditions-and-views.sql
@@ -1,51 +1,30 @@
+/*
+Задача : порахувати кількість замовлень
+*/
 
--- Вивести інфу про всіх юзерів + скільки хто зробив замовлень
+CREATE MATERIALIZED VIEW total_orders AS (
+    SELECT count(*) AS "загальна кількість замовлень" 
+    FROM orders
+)
 
-SELECT u.id, u.first_name, u.last_name, u.email, count(o.id) AS "orders_amount" FROM 
-users AS u LEFT JOIN orders AS o
-ON u.id = o.customer_id
-GROUP BY u.id
-ORDER BY "orders_amount";
+SELECT * FROM total_orders; -- 2983 замовлення
 
+INSERT INTO orders (customer_id, status)
+VALUES (3000, false);
 
-CREATE VIEW users_with_orders_amount AS (
-        SELECT u.id, u.first_name, u.last_name, u.email, count(o.id) AS "orders_amount" FROM 
-        users AS u LEFT JOIN orders AS o
-        ON u.id = o.customer_id
-        GROUP BY u.id
-        ORDER BY "orders_amount"
-);
-
-SELECT * FROM users_with_orders_amount;
-
--- Отримати email юзерів, які мають менше 2 замовлень
-
-SELECT email, orders_amount FROM users_with_orders_amount
-WHERE orders_amount < 2;
-
--- View можна видаляти 
-
-DROP VIEW users_with_orders_amount;
-
--- Створити представлення, яке зберігає замовлення з їхньою вартістю
--- Додати до цієї view інформацію про замовлення (id замовника)
-
-CREATE VIEW orders_with_price AS (
-        SELECT o.id, o.customer_id, sum(p.price * otp.quantity) AS "order_sum", o.status FROM 
-        orders AS o JOIN orders_to_products AS otp
-        ON o.id = otp.order_id
-        JOIN products AS p
-        ON p.id = otp.products_id
-        GROUP BY o.id
-);
-
-DROP VIEW orders_with_price;
+REFRESH MATERIALIZED VIEW total_orders;
 
 
--- Вивести юзерів з сумою коштів, яку вони витратили у нашому магазині
 
-SELECT  u.id, u.email, sum(owp.order_sum) AS "sum" FROM 
-users AS u LEFT JOIN orders_with_price AS owp
-ON u.id = owp.customer_id
-GROUP BY u.id
-ORDER BY sum;
+CREATE FUNCTION refresh_materialized_view ()
+RETURNS void
+AS
+$$
+BEGIN
+    REFRESH MATERIALIZED VIEW total_orders;
+END;
+$$
+LANGUAGE plpgsql;
+
+SELECT refresh_materialized_view ();
+
